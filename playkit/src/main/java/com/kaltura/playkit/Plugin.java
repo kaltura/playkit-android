2e2f04e1f6509392cc3a478bbdc22cea34a4d3df
Noam Tamim <noam.tamim@kaltura.com>
2016-10-30 21:48:34 +0200

    Adjusted order of params.

--- playkit/src/main/java/com/kaltura/playkit/Plugin.java
+++ playkit/src/main/java/com/kaltura/playkit/Plugin.java
@@ -4,13 +4,7 @@ import android.content.Context;
 
 public abstract class Plugin {
 
-    /**
-     * Called by the library to load the plugin, providing it with a player. The plugin should use
-     * this method to 
-     * @param context
-     * @param player
-     */
-    protected abstract void load(Context context, PlayerConfig playerConfig, Player player);
+    protected abstract void load(Player player, PlayerConfig playerConfig, Context context);
 
     protected abstract void release();
 
