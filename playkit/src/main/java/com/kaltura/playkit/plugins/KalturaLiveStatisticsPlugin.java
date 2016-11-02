c593b2b520d5137efced3c8d93a28d202861a16a
Ziv Ilan <ziv.ilan@kaltura.com>
2016-11-02 16:31:10 +0200

    adding all plugins base classes

--- playkit/src/main/java/com/kaltura/playkit/plugins/KalturaLiveStatisticsPlugin.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/KalturaLiveStatisticsPlugin.java
@@ -1,8 +1,79 @@
 package com.kaltura.playkit.plugins;
 
+import android.content.Context;
+
+import com.kaltura.playkit.Player;
+import com.kaltura.playkit.PlayerConfig;
+import com.kaltura.playkit.PlayerEvent;
+import com.kaltura.playkit.Plugin;
+
 /**
  * Created by zivilan on 02/11/2016.
  */
 
-public class KalturaLiveStatisticsPlugin {
+public class KalturaLiveStatisticsPlugin extends Plugin {
+    private static final String TAG = "KalturaLiveStatisticsPlugin";
+
+    public static final Factory factory = new Factory() {
+        @Override
+        public String getName() {
+            return "KalturaLiveStatistics";
+        }
+
+        @Override
+        public Plugin newInstance() {
+            return new KalturaLiveStatisticsPlugin();
+        }
+    };
+
+    @Override
+    protected void load(Player player, PlayerConfig playerConfig, Context context) {
+        player.addEventListener(mEventListener);
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    private PlayerEvent.Listener mEventListener = new PlayerEvent.Listener() {
+        @Override
+        public void onPlayerEvent(Player player, PlayerEvent event) {
+            switch (event){
+                case CAN_PLAY:
+
+                    break;
+                case DURATION_CHANGE:
+
+                    break;
+                case ENDED:
+
+                    break;
+                case ERROR:
+
+                    break;
+                case LOADED_METADATA:
+
+                    break;
+                case PAUSE:
+
+                    break;
+                case PLAY:
+
+                    break;
+                case PLAYING:
+
+                    break;
+                case SEEKED:
+
+                    break;
+                case SEEKING:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    };
 }
--- playkit/src/main/java/com/kaltura/playkit/plugins/KalturaStatisticsPlugin.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/KalturaStatisticsPlugin.java
@@ -1,8 +1,80 @@
 package com.kaltura.playkit.plugins;
 
+import android.content.Context;
+
+import com.kaltura.playkit.Player;
+import com.kaltura.playkit.PlayerConfig;
+import com.kaltura.playkit.PlayerEvent;
+import com.kaltura.playkit.Plugin;
+
 /**
  * Created by zivilan on 02/11/2016.
  */
 
-public class KalturaStatisticsPlugin {
+public class KalturaStatisticsPlugin extends Plugin {
+    private static final String TAG = "KalturaStatisticsPlugin";
+
+    public static final Factory factory = new Factory() {
+        @Override
+        public String getName() {
+            return "KalturaStatistics";
+        }
+
+        @Override
+        public Plugin newInstance() {
+            return new KalturaStatisticsPlugin();
+        }
+    };
+
+    @Override
+    protected void load(Player player, PlayerConfig playerConfig, Context context) {
+        player.addEventListener(mEventListener);
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    private PlayerEvent.Listener mEventListener = new PlayerEvent.Listener() {
+        @Override
+        public void onPlayerEvent(Player player, PlayerEvent event) {
+            switch (event){
+                case CAN_PLAY:
+
+                    break;
+                case DURATION_CHANGE:
+
+                    break;
+                case ENDED:
+
+                    break;
+                case ERROR:
+
+                    break;
+                case LOADED_METADATA:
+
+                    break;
+                case PAUSE:
+
+                    break;
+                case PLAY:
+
+                    break;
+                case PLAYING:
+
+                    break;
+                case SEEKED:
+
+                    break;
+                case SEEKING:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    };
+
 }
