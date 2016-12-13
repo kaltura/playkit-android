45db1c00957a1be48081811e343510e029fc3494
Itan Bar Peled <itan.barpeled@kaltura.com>
2016-12-13 14:16:13 +0200

    mock data

--- playkit/src/main/java/com/kaltura/playkit/addon/cast/GoogleCastAddOn.java
+++ playkit/src/main/java/com/kaltura/playkit/addon/cast/GoogleCastAddOn.java
@@ -29,25 +29,10 @@ public class GoogleCastAddOn {
 
     // TODO itan - remove all this temps from here
     private static final String MOCK_DATA = "MOCK_DATA";
-    private static final String TEMP_LIB_URL_OVP = "http://player-227562931.eu-west-1.elb.amazonaws.com/v2.51.receiver.rc1/mwEmbed/";
-    private static final String TEMP_LIB_URL_OTT = "";
-    private static final String MOCK_INIT_OBJECT = "{\n" +
-            "\"initObj\": {\n" +
-            "\"Locale\": {\n" +
-            "\"LocaleDevice\": null,\n" +
-            "\"LocaleLanguage\": \"en\",\n" +
-            "\"LocaleCountry\": null,\n" +
-            "\"LocaleUserState\": \"Unknown\"\n" +
-            "},\n" +
-            "\"UDID\": \"2345\",\n" +
-            "\"ApiUser\": \"tvpapi_198\",\n" +
-            "\"DomainID\": 362595,\n" +
-            "\"ApiPass\": \"11111\",\n" +
-            "\"SiteGuid\": \"739182\",\n" +
-            "\"Platform\": \"Web\"\n" +
-            "}\n" +
-            "}";
-    private static final String MOCK_AD_TAG_URL = "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator=";
+    private static final String TEMP_LIB_URL_OVP = "https://kgit.html5video.org/pulls/3156/";
+    private static final String TEMP_LIB_URL_LOCAL = "http://192.168.162.231/html5.kaltura/mwEmbed/";
+    private static final String TEMP_LIB_URL_OTT = "http://player-227562931.eu-west-1.elb.amazonaws.com/v2.51.receiver.rc2/mwEmbed/";
+    private static final String TEMP_AD_TAG_URL = "http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator=";
 
 
 
@@ -55,16 +40,35 @@ public class GoogleCastAddOn {
     public static void getMediaInfoBuilder(OnCompletion<MediaInfo.Builder> completionListener) {
 
 
+
         JSONObject customData = new JSONObject();
         JSONObject embedConfig = new JSONObject();
 
         try {
 
+
+            // OVP
+            String entryId = "0_l1v5vzh3";
             embedConfig.put("lib", TEMP_LIB_URL_OVP);
-            embedConfig.put("publisherID", "243342"); //"198"
-            embedConfig.put("uiconfID", "21099702"); //21099702
-            embedConfig.put("entryID", "0_l1v5vzh3"); //258656
-            //embedConfig.put("flashVars", getFlashVars("", MOCK_AD_TAG_URL));
+            embedConfig.put("publisherID", "243342");
+            embedConfig.put("uiconfID", "21099702");
+            embedConfig.put("entryID", entryId);
+            //embedConfig.put("flashVars", getFlashVars(null, TEMP_AD_TAG_URL, null, null));
+
+
+
+
+            // OTT
+            /*
+            String entryId = "258656";
+            embedConfig.put("lib", TEMP_LIB_URL_OTT);
+            embedConfig.put("publisherID", "198");
+            embedConfig.put("uiconfID", "8413355");
+            embedConfig.put("entryID", entryId);
+            embedConfig.put("flashVars", getFlashVars("temp_ks", TEMP_AD_TAG_URL, "Web SD", entryId));
+            */
+
+
 
             customData.put("embedConfig", embedConfig);
 
@@ -96,10 +100,44 @@ public class GoogleCastAddOn {
     }
 
 
+
+    private static JSONObject getMockInitObject() {
+
+        JSONObject initObj = new JSONObject();
+        JSONObject local = new JSONObject();
+
+        try {
+
+            local.put("LocaleDevice", "");
+            local.put("LocaleLanguage", "en");
+            local.put("LocaleCountry", "");
+            local.put("LocaleUserState", "Unknown");
+            initObj.put("Locale", local);
+
+            initObj.put("UDID", "2345");
+            initObj.put("ApiUser", "tvpapi_198");
+            initObj.put("DomainID", "362595");
+            initObj.put("ApiPass", "11111");
+            initObj.put("SiteGuid", "739182");
+            initObj.put("Platform", "Web");
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return initObj;
+
+    }
+
+
     public static void getMediaInfoBuilder(PlayerConfig playerConfig, SessionProvider sessionProvider,
                                            final String uiConf, final String fileFormat,
                                            final OnCompletion<MediaInfo.Builder> completionListener) {
 
+        // initObj
+        // base url
 
         final String entryId = getEntryId(playerConfig);
         final int partnerId = getPartnerId(sessionProvider);
@@ -224,7 +262,7 @@ public class GoogleCastAddOn {
             embedConfig.put("publisherID", partnerId);
             embedConfig.put("uiconfID", uiConf);
             embedConfig.put("entryID", entryId);
-            embedConfig.put("flashVars", getFlashVars(ks, adTagUrl, fileFormat));
+            embedConfig.put("flashVars", getFlashVars(ks, adTagUrl, fileFormat, entryId));
             //setFileFormat(embedConfig, fileFormat);
 
 
@@ -254,11 +292,11 @@ public class GoogleCastAddOn {
 
 
 
-    private static JSONObject getFlashVars(String ks, String adTagUrl, String fileFormat) {
+    private static JSONObject getFlashVars(String ks, String adTagUrl, String fileFormat, String entryId) {
 
         JSONObject flashVars = new JSONObject();
 
-        setProxyData(flashVars, ks, fileFormat);
+        setProxyData(flashVars, ks, fileFormat, entryId);
         setDoubleClickPlugin(flashVars, adTagUrl);
 
         return flashVars;
@@ -267,10 +305,7 @@ public class GoogleCastAddOn {
 
 
 
-    //flashVars.put("ks", "1234");
-    //flashVars.put("proxyData", MOCK_INIT_OBJECT);
-
-    private static void setProxyData(JSONObject flashVars, String ks, String fileFormat) {
+    private static void setProxyData(JSONObject flashVars, String ks, String fileFormat, String entryId) {
 
         JSONObject proxyData = new JSONObject();
         JSONObject configObject = new JSONObject();
@@ -281,10 +316,14 @@ public class GoogleCastAddOn {
 
 
                 configObject.put("flavorassets", new JSONObject().put("filters", new JSONObject().put("include", new JSONObject().put("Format", new JSONArray().put(fileFormat)))));
+                configObject.put("baseentry", new JSONObject().put("vars", new JSONObject().put("isTrailer", "" + false)));
                 proxyData.put("config", configObject);
 
-                proxyData.put("initObj", MOCK_INIT_OBJECT);
+                proxyData.put("initObj", getMockInitObject());
                 //proxyData.put("ks", ks);
+
+                proxyData.put("MediaID", entryId);
+                proxyData.put("iMediaID", entryId);
                 flashVars.put("proxyData", proxyData);
 
             } catch(JSONException e) {
