da0b7075c648befdcfa4efabe953314f92d40be4
Gilad Nadav <Gilad.Nadav@kaltura.com>
2016-12-15 11:18:00 +0200

    fix API to AdController

--- playkit/src/androidTest/java/com/kaltura/playkit/plugins/MockPlayer.java
+++ playkit/src/androidTest/java/com/kaltura/playkit/plugins/MockPlayer.java
@@ -7,7 +7,7 @@ import android.view.View;
 import com.kaltura.playkit.PKEvent;
 import com.kaltura.playkit.Player;
 import com.kaltura.playkit.PlayerConfig;
-import com.kaltura.playkit.ads.PKAdInfo;
+import com.kaltura.playkit.ads.AdController;
 
 /**
  * Created by zivilan on 11/12/2016.
@@ -121,7 +121,7 @@ public class MockPlayer implements Player {
     }
 
     @Override
-    public PKAdInfo getAdInfo() {
+    public AdController getAdController() {
         return null;
     }
 }
