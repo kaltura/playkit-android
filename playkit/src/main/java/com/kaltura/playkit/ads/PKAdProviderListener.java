3ce887d02eca8d83611d663e2e305792d6a55eab
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-02-09 23:15:32 +0200

    VE-80 LOAD IMA Then Content

---
+++ playkit/src/main/java/com/kaltura/playkit/ads/PKAdProviderListener.java
@@ -0,0 +1,8 @@
+package com.kaltura.playkit.ads;
+
+/**
+ * Created by gilad.nadav on 25/01/2017.
+ */
+public interface PKAdProviderListener {
+    void onAdLoadingFinished(PKPrepareReason pkPrepareReason);
+}
