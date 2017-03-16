46a47de6947f079e3ca9752b671f9d0c177b598f
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-03-16 08:01:08 +0200

    Add event AD_LOAD_TIMEOUT_TIMER_STARTED

--- playkit/src/main/java/com/kaltura/playkit/plugins/ads/AdEvent.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/ads/AdEvent.java
@@ -60,6 +60,7 @@ public class AdEvent implements PKEvent {
         LOADED,
         CONTENT_PAUSE_REQUESTED,
         CONTENT_RESUME_REQUESTED,
+        AD_LOAD_TIMEOUT_TIMER_STARTED,
         ALL_ADS_COMPLETED
     }
 
