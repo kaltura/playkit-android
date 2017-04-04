684d5d9728c58d771ec212ea0099251066b2d4dd
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-04-04 10:44:45 +0300

    remove player null check

--- playkit/src/main/java/com/kaltura/playkit/plugins/KalturaLiveStatsPlugin.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/KalturaLiveStatsPlugin.java
@@ -212,7 +212,7 @@ public class KalturaLiveStatsPlugin extends PKPlugin {
 
 
     private void sendLiveEvent(final long bufferTime) {
-        String sessionId = (player != null && player.getSessionId() != null) ? player.getSessionId() : "";
+        String sessionId = (player.getSessionId() != null) ? player.getSessionId() : "";
         String baseUrl = pluginConfig.has("baseUrl") ? pluginConfig.getAsJsonPrimitive("baseUrl").getAsString() : BASE_URL;
         int partnerId = pluginConfig.has("partnerId") ? pluginConfig.getAsJsonPrimitive("partnerId").getAsInt() : 0;
         String entryId = pluginConfig.has("entryId") ? pluginConfig.getAsJsonPrimitive("entryId").getAsString() : mediaConfig.getMediaEntry().getId();
