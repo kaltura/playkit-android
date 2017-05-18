f225db588155c2c888a274a6c528691c9ea50f27
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-05-18 10:17:38 +0300

    preparation for supporting config object for Kaltura stats and stats live

--- playkit/src/main/java/com/kaltura/playkit/plugins/configs/KalturaStatsConfig.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/configs/KalturaStatsConfig.java
@@ -1,13 +1,14 @@
 package com.kaltura.playkit.plugins.configs;
 
 import com.google.gson.JsonObject;
+import com.kaltura.playkit.utils.Consts;
 
 /**
  * Created by gilad.nadav on 18/05/2017.
  */
 
 public class KalturaStatsConfig {
-    private static final int DEFAULT_TIMER_INTERVAL_MILLIS = 30000;
+
 
     public static final String PARTNER_ID = "partnerId";
     public static final String UICONF_ID  = "uiconfId";
@@ -15,26 +16,34 @@ public class KalturaStatsConfig {
     public static final String BASE_URL   = "baseUrl";
     public static final String TIMER_INTERVAL_MILLIS = "timerIntervalMillis";
 
-    private String partnerId;
-    private String uiconfId;
+    private int partnerId;
+    private int uiconfId;
     private String entryId;
     private String baseUrl;
     private int timerIntervalMillis;
 
-    public KalturaStatsConfig(String uiconfId, String partnerId, String entryId) {
-        this.timerIntervalMillis = DEFAULT_TIMER_INTERVAL_MILLIS;
+    public KalturaStatsConfig(int uiconfId, int partnerId, String entryId) {
+        this.timerIntervalMillis = Consts.DEFAULT_ANALYTICS_TIMER_INTERVAL_LOW;
         this.baseUrl = "https://stats.kaltura.com/api_v3/index.php";
         this.partnerId = partnerId;
         this.uiconfId = uiconfId;
         this.entryId = entryId;
     }
 
-    public KalturaStatsConfig setPartnerId(String partnerId) {
+    public KalturaStatsConfig(int uiconfId, int partnerId, String entryId, String baseUrl, int timerIntervalMillis) {
+        this.timerIntervalMillis = timerIntervalMillis;
+        this.baseUrl = baseUrl;
+        this.partnerId = partnerId;
+        this.uiconfId = uiconfId;
+        this.entryId = entryId;
+    }
+
+    public KalturaStatsConfig setPartnerId(int partnerId) {
         this.partnerId = partnerId;
         return this;
     }
 
-    public KalturaStatsConfig setUiconfId(String uiconfId) {
+    public KalturaStatsConfig setUiconfId(int uiconfId) {
         this.uiconfId = uiconfId;
         return this;
     }
@@ -54,10 +63,30 @@ public class KalturaStatsConfig {
         return this;
     }
 
+    public int getPartnerId() {
+        return partnerId;
+    }
+
+    public int getUiconfId() {
+        return uiconfId;
+    }
+
+    public String getEntryId() {
+        return entryId;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public int getTimerIntervalMillis() {
+        return timerIntervalMillis;
+    }
+
     public JsonObject toJSONObject() {
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty(UICONF_ID, uiconfId);
         jsonObject.addProperty(PARTNER_ID, partnerId);
+        jsonObject.addProperty(UICONF_ID, uiconfId);
         jsonObject.addProperty(ENTRY_ID, entryId);
         jsonObject.addProperty(BASE_URL, baseUrl);
         jsonObject.addProperty(TIMER_INTERVAL_MILLIS, timerIntervalMillis);
