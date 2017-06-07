9b25a75081f389273dfcb64ada0f795886b4bf33
Noam Tamim <noam.tamim@kaltura.com>
2017-06-08 00:21:53 +0300

    Update PhoenixMediaProvider.java

--- playkit/src/main/java/com/kaltura/playkit/mediaproviders/ott/PhoenixMediaProvider.java
+++ playkit/src/main/java/com/kaltura/playkit/mediaproviders/ott/PhoenixMediaProvider.java
@@ -418,7 +418,7 @@ public class PhoenixMediaProvider extends BEMediaProvider {
 
             PKMediaEntry mediaEntry = new PKMediaEntry();
             mediaEntry.setId("" + assetId);
-            mediaEntry.setName(null); // media title is not sent in ott requests only in ovp requests
+            mediaEntry.setName(null);
 
             // until the response will be delivered in the right order:
             playbackSourcesSort(sourcesFilter, playbackSources);
