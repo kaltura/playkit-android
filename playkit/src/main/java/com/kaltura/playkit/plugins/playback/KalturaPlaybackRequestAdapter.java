795bf8ec09e0f05fcc984cea167b4ab53df9a692
Noam Tamim <noamtm@gmail.com>
2017-06-13 00:41:08 +0300

    KalturaPlaybackRequestAdapter: add a referrer parameter with package name.

--- playkit/src/main/java/com/kaltura/playkit/plugins/playback/KalturaPlaybackRequestAdapter.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/playback/KalturaPlaybackRequestAdapter.java
@@ -1,34 +1,41 @@
 package com.kaltura.playkit.plugins.playback;
 
+import android.content.Context;
 import android.net.Uri;
+import android.util.Base64;
 
 import com.kaltura.playkit.PKRequestParams;
 import com.kaltura.playkit.Player;
 
+import static android.util.Base64.NO_WRAP;
 import static com.kaltura.playkit.PlayKitManager.CLIENT_TAG;
 
 /**
  * Created by Noam Tamim @ Kaltura on 28/03/2017.
  */
 public class KalturaPlaybackRequestAdapter implements PKRequestParams.Adapter {
-    
+
+    private final String packageName;
     private String playSessionId;
     
-    public static void setup(Player player) {
-        KalturaPlaybackRequestAdapter decorator = new KalturaPlaybackRequestAdapter(player.getSessionId());
+    public static void setup(Context context, Player player) {
+        KalturaPlaybackRequestAdapter decorator = new KalturaPlaybackRequestAdapter(context.getPackageName(), player.getSessionId());
         player.getSettings().setContentRequestAdapter(decorator);
     }
 
-    private KalturaPlaybackRequestAdapter(String playSessionId) {
+    private KalturaPlaybackRequestAdapter(String packageName, String playSessionId) {
+        this.packageName = packageName;
         this.playSessionId = playSessionId;
     }
     
     @Override
     public PKRequestParams adapt(PKRequestParams requestParams) {
         Uri url = requestParams.url;
+
         if (url.getPath().contains("/playManifest/")) {
             Uri alt = url.buildUpon()
                     .appendQueryParameter("clientTag", CLIENT_TAG)
+                    .appendQueryParameter("referrer", Base64.encodeToString(packageName.getBytes(), NO_WRAP))
                     .appendQueryParameter("playSessionId", playSessionId)
                     .build();
             return new PKRequestParams(alt, requestParams.headers);
