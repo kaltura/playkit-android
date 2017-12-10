b565b8cf80027b15c7c8b855191004a8509a087b
AntonAFA <anton.afanasiev@kaltura.com>
2017-12-10 17:37:35 +0200

    Fem 1782 (#335)
    * FEM-1782
    
    Change the way we import exoplayer library. Import only needed libraries.
    
    * - remove googlecast from playkit dependencies
    
    * FEM-1782
    
    + Fix deprecated Exoplayer.newSimpleInstance() builder to the new one
    
    * FEM-1782
    
    fix deprecated exoplayer methods/interfaces

--- playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
+++ playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
@@ -20,7 +20,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -31,6 +31,7 @@ import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.metadata.Metadata;
+import com.google.android.exoplayer2.metadata.MetadataOutput;
 import com.google.android.exoplayer2.metadata.MetadataRenderer;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
@@ -80,7 +81,7 @@ import static com.kaltura.playkit.utils.Consts.TRACK_TYPE_TEXT;
 /**
  * Created by anton.afanasiev on 31/10/2016.
  */
-class ExoPlayerWrapper implements PlayerEngine, ExoPlayer.EventListener, MetadataRenderer.Output, BandwidthMeter.EventListener {
+class ExoPlayerWrapper implements PlayerEngine, Player.EventListener, MetadataOutput, BandwidthMeter.EventListener {
 
     private static final PKLog log = PKLog.get("ExoPlayerWrapper");
 
@@ -154,7 +155,9 @@ class ExoPlayerWrapper implements PlayerEngine, ExoPlayer.EventListener, Metadat
 
         DefaultTrackSelector trackSelector = initializeTrackSelector();
         drmSessionManager = new DeferredDrmSessionManager(mainHandler, buildHttpDataSourceFactory(false), drmSessionListener);
-        player = ExoPlayerFactory.newSimpleInstance(context, trackSelector, new DefaultLoadControl(), drmSessionManager);
+        DefaultRenderersFactory renderersFactory = new DefaultRenderersFactory(context,
+                drmSessionManager, DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF);
+        player = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector);
         window = new Timeline.Window();
         setPlayerListeners();
         exoPlayerView.setPlayer(player);
@@ -167,7 +170,7 @@ class ExoPlayerWrapper implements PlayerEngine, ExoPlayer.EventListener, Metadat
             player.addListener(eventLogger);
             player.setVideoDebugListener(eventLogger);
             player.setAudioDebugListener(eventLogger);
-            player.setMetadataOutput(this);
+            player.addMetadataOutput(this);
         }
     }
 
