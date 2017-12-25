6f7ede7898ddb12d77aff14074349affeb355420
anton.afanasiev <anton.afanasiev@kaltura@com>
2017-12-25 16:18:42 +0200

    + tmp commit

--- playkit/src/main/java/com/kaltura/playkit/PKDeviceCapabilities.java
+++ playkit/src/main/java/com/kaltura/playkit/PKDeviceCapabilities.java
@@ -54,9 +54,11 @@ public class PKDeviceCapabilities {
     private static final String PREFS_ENTRY_FINGERPRINT = "Build.FINGERPRINT";
     private static final String DEVICE_CAPABILITIES_URL = "https://cdnapisec.kaltura.com/api_v3/index.php?service=stats&action=reportDeviceCapabilities";
     private static final String FINGERPRINT = Build.FINGERPRINT;
-    private static final String[] H265_HARDWARE_CODECS_LIST = {"OMX.qualcomm.hevc.decoder", "OMX.MEDIATEK.HEVC.DECODER", "OMX.Exynos.hevc.dec"};     //OMX.qcom.video.decoder.hevc --> HW or SW???
 
-    private static final String[] H265_SOFTWARE_CODECS_LIST = {"OMX.google.hevc.decoder", "OMX.SEC.hevc.sw.dec"};
+    private static final String[] H265_HARDWARE_CODECS_LIST = {"OMX.qualcomm.hevc.decoder", "OMX.MEDIATEK.HEVC.DECODER", "OMX.Exynos.hevc.dec"};     //OMX.qcom.video.decoder.hevc --> HW or SW???
+                                                                                                                                                    // OMX.Exynos.hevc.dec.secure ---> SECURE? investigate!
+
+    private static final String[] H265_SOFTWARE_CODECS_LIST = {"OMX.google.hevc.decoder", "OMX.SEC.hevc.sw.dec", "OMX.IMG.MSVDX.Decoder.HEVC"};      //OMX.SEC.hevc.sw.dec --> 'SEC' is for secured?
     private static boolean reportSent = false;
 
     private final Context context;
