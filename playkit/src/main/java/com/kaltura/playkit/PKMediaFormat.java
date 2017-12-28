b57a4f729494962bc7107cffbb6c3b1f5ceda1c1
Gilad Nadav <Gilad.Nadav@kaltura.com>
2017-12-28 14:26:16 +0200

    add contain method to PKMeidaFormat so we can check if we got supported format to play

--- playkit/src/main/java/com/kaltura/playkit/PKMediaFormat.java
+++ playkit/src/main/java/com/kaltura/playkit/PKMediaFormat.java
@@ -43,6 +43,15 @@ public enum PKMediaFormat {
         this.pathExt = pathExt;
     }
 
+    public static boolean contains(String playbackFormat) {
+        for (PKMediaFormat format : PKMediaFormat.values()) {
+            if (format.name().equals(playbackFormat)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static PKMediaFormat valueOfExt(String ext) {
         return extensionLookup.get(ext);
     }
