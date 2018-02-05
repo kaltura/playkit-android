6494832675e7f82c8359bc7aed08bde1ebfe9d90
Gilad Nadav <gilad.nadav@kaltura.com>
2018-02-05 17:46:48 +0200

    codacy

--- playkit/src/main/java/com/kaltura/playkit/player/SourceSelector.java
+++ playkit/src/main/java/com/kaltura/playkit/player/SourceSelector.java
@@ -24,7 +24,6 @@ import com.kaltura.playkit.PKMediaFormat;
 import com.kaltura.playkit.PKMediaSource;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
