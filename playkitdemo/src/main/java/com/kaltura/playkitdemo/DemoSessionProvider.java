b793ee43686d588cb2ede080da7cf64cfed83d4f
Gilad Nadav <gilad.nadav@kaltura.com>
2018-07-01 17:31:19 +0300

    disable demo till dev is built (#422)

--- playkitdemo/src/main/java/com/kaltura/playkitdemo/DemoSessionProvider.java
+++ playkitdemo/src/main/java/com/kaltura/playkitdemo/DemoSessionProvider.java
@@ -1,47 +1,51 @@
 package com.kaltura.playkitdemo;
 
-import com.kaltura.netkit.connect.response.PrimitiveResult;
-import com.kaltura.netkit.utils.OnCompletion;
-import com.kaltura.netkit.utils.SessionProvider;
+//import com.kaltura.netkit.connect.response.PrimitiveResult;
+//import com.kaltura.netkit.utils.OnCompletion;
+//import com.kaltura.netkit.utils.SessionProvider;
 
 /**
  * @hide
  */
 
-public class DemoSessionProvider implements SessionProvider {
+public class DemoSessionProvider {
 
-    private String ks;
-
-    private static DemoSessionProvider self;
-
-    public static DemoSessionProvider getSessionProvider(){
-        if(self == null){
-            self = new DemoSessionProvider();
-        }
-        return self;
-    }
-
-    private DemoSessionProvider(){
-    }
-
-    public void setKs(String ks) {
-        this.ks = ks;
-    }
-
-    @Override
-    public String baseUrl() {
-        return MockParams.PhoenixBaseUrl;
-    }
-
-    @Override
-    public void getSessionToken(OnCompletion<PrimitiveResult> completion) {
-        if (completion!=null){
-            completion.onComplete(new PrimitiveResult(this.ks));
-        }
-    }
-
-    @Override
-    public int partnerId() {
-        return MockParams.OttPartnerId;
-    }
 }
+
+//public class DemoSessionProvider implements SessionProvider {
+//
+//    private String ks;
+//
+//    private static DemoSessionProvider self;
+//
+//    public static DemoSessionProvider getSessionProvider(){
+//        if(self == null){
+//            self = new DemoSessionProvider();
+//        }
+//        return self;
+//    }
+//
+//    private DemoSessionProvider(){
+//    }
+//
+//    public void setKs(String ks) {
+//        this.ks = ks;
+//    }
+//
+//    @Override
+//    public String baseUrl() {
+//        return MockParams.PhoenixBaseUrl;
+//    }
+//
+//    @Override
+//    public void getSessionToken(OnCompletion<PrimitiveResult> completion) {
+//        if (completion!=null){
+//            completion.onComplete(new PrimitiveResult(this.ks));
+//        }
+//    }
+//
+//    @Override
+//    public int partnerId() {
+//        return MockParams.OttPartnerId;
+//    }
+//}
