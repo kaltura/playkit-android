dd40bbb3c33e76b37bc26676a61d35c67b384354
Gilad Nadav <gilad.nadav@kaltura.com>
2018-08-19 16:16:19 +0300

    fix change media outOfBund Exception in ExoPlayer when changing media between dash to m3u8

--- playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
+++ playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
@@ -696,6 +696,7 @@ class ExoPlayerWrapper implements PlayerEngine, Player.EventListener, MetadataOu
     @Override
     public void stop() {
         log.d("stop");
+        shouldResetPlayerPosition = true;
         preferredLanguageWasSelected = false;
         lastKnownVolume = Consts.DEFAULT_VOLUME;
         lastKnownPlaybackRate = Consts.DEFAULT_PLAYBACK_RATE_SPEED;
@@ -704,7 +705,7 @@ class ExoPlayerWrapper implements PlayerEngine, Player.EventListener, MetadataOu
             trackSelectionHelper.stop();
         }
         if (player != null) {
-            player.stop();
+            player.stop(true);
             sendDistinctEvent(PlayerEvent.Type.STOPPED);
         }
     }
