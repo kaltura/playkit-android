c14e6f1b8334e2e7fa19b11b57ed97891241c19c
Gilad Nadav <gilad.nadav@kaltura.com>
2018-10-30 18:26:00 +0200

    make sure that the case here is MPGA by checking that the id contains format "1/15"

--- playkit/src/main/java/com/kaltura/playkit/player/TrackSelectionHelper.java
+++ playkit/src/main/java/com/kaltura/playkit/player/TrackSelectionHelper.java
@@ -174,9 +174,11 @@ class TrackSelectionHelper {
                                 if (!isDashManifest) {
                                     audioTrackLabel = format.id;
                                 }
-                                if (format.language == null) { // filtering audio tracks that their language is null
-                                    if (mpgaAudioFormatEnabled) {
-                                        audioTracks.add(new AudioTrack(uniqueId, format.id, audioTrackLabel, format.bitrate, format.selectionFlags, false));
+                                if (format.language == null) {
+                                    if (format.id != null && format.id.matches("\\d+/\\d+")) {
+                                        if (mpgaAudioFormatEnabled) {
+                                            audioTracks.add(new AudioTrack(uniqueId, format.id, audioTrackLabel, format.bitrate, format.selectionFlags, false));
+                                        }
                                     }
                                 } else {
                                     audioTracks.add(new AudioTrack(uniqueId, getLanguageFromFormat(format), audioTrackLabel, format.bitrate, format.selectionFlags, false));
