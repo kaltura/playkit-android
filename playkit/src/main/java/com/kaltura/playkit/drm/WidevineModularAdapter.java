3a3ebf4787bb60b3602d3288f6d2d8a9894750d2
Gilad Nadav <gilad.nadav@kaltura.com>
2018-11-08 10:49:05 +0200

    cosmetics

--- playkit/src/main/java/com/kaltura/playkit/drm/WidevineModularAdapter.java
+++ playkit/src/main/java/com/kaltura/playkit/drm/WidevineModularAdapter.java
@@ -169,7 +169,7 @@ class WidevineModularAdapter extends DrmAdapter {
         FrameworkMediaDrm mediaDrm = createMediaDrm();
         FrameworkMediaDrm.KeyRequest releaseRequest;
         try {
-            releaseRequest = mediaDrm.getKeyRequest(keySetId, null ,MediaDrm.KEY_TYPE_RELEASE,null);
+            releaseRequest = mediaDrm.getKeyRequest(keySetId, null, MediaDrm.KEY_TYPE_RELEASE,null);
         } catch (NotProvisionedException e) {
             throw new WidevineNotSupportedException(e);
         }
