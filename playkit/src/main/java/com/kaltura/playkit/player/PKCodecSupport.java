e319219d02330e0b088b89632d90092846af6e4d
Gilad Nadav <gilad.nadav@kaltura.com>
2018-11-14 10:01:20 +0200

    Null is not expected here

--- playkit/src/main/java/com/kaltura/playkit/player/PKCodecSupport.java
+++ playkit/src/main/java/com/kaltura/playkit/player/PKCodecSupport.java
@@ -3,7 +3,6 @@ package com.kaltura.playkit.player;
 import android.media.MediaCodecInfo;
 import android.media.MediaCodecList;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.google.android.exoplayer2.Format;
@@ -28,7 +27,7 @@ public class PKCodecSupport {
 
     private static boolean isCodecSupportedInternal(String codec, TrackType type) {
         String mimeType = (type == TrackType.AUDIO) ? MimeTypes.getAudioMediaMimeType(codec) :
-                                               MimeTypes.getVideoMediaMimeType(codec);
+                MimeTypes.getVideoMediaMimeType(codec);
 
         for (int i = 0, codecCount = MediaCodecList.getCodecCount(); i < codecCount; i++) {
             final MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
@@ -59,8 +58,7 @@ public class PKCodecSupport {
         return sup;
     }
 
-    public static boolean isFormatSupported(@NonNull Format format, @Nullable TrackType type) {
-
+    public static boolean isFormatSupported(@NonNull Format format, @NonNull TrackType type) {
         if (type == TrackType.TEXT) {
             return true;    // always supported
         }
@@ -70,20 +68,6 @@ public class PKCodecSupport {
             return true;
         }
 
-        if (type == null) {
-            // type==null: HLS muxed track with a <video,audio> tuple
-            final String[] split = TextUtils.split(format.codecs, ",");
-            boolean result = true;
-            switch (split.length) {
-                case 0: return false;
-                case 2: result = isCodecSupported(split[1], TrackType.AUDIO);
-                // fallthrough
-                case 1: result &= isCodecSupported(split[0], TrackType.VIDEO);
-            }
-            return result;
-
-        } else {
-            return isCodecSupported(format.codecs, type);
-        }
+        return isCodecSupported(format.codecs, type);
     }
 }
