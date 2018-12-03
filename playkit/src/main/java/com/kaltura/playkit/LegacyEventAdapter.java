6c719742e7bf6e3b260f15d77e16f4016b76135b
Noam Tamim <noam.tamim@kaltura.com>
2018-12-03 23:45:47 +0200

    Moves and cleanups

---
+++ playkit/src/main/java/com/kaltura/playkit/LegacyEventAdapter.java
@@ -0,0 +1,403 @@
+package com.kaltura.playkit;
+
+import com.kaltura.playkit.player.AudioTrack;
+import com.kaltura.playkit.player.PKTracks;
+import com.kaltura.playkit.player.TextTrack;
+import com.kaltura.playkit.player.VideoTrack;
+import com.kaltura.playkit.player.metadata.PKMetadata;
+import com.kaltura.playkit.plugins.ads.AdCuePoints;
+import com.kaltura.playkit.plugins.ads.AdEvent;
+import com.kaltura.playkit.plugins.ads.AdInfo;
+
+import java.util.List;
+
+class LegacyEventAdapter {
+
+    private final MessageBus messageBus;
+
+    final PKEvent.Listener legacyToNewAdsEvents = event -> {
+        final AdEvent e = (AdEvent) event;
+        switch (e.type) {
+
+            case AD_REQUESTED:
+                postAdsEvent(L -> L.onAdRequested(((AdEvent.AdRequestedEvent) e).adTagUrl));
+                break;
+            case AD_FIRST_PLAY:
+                postAdsEvent(AdsListener::onAdFirstPlay);
+                break;
+            case STARTED:
+                postAdsEvent(L -> L.onAdStarted(((AdEvent.AdStartedEvent) e).adInfo));
+                break;
+            case AD_DISPLAYED_AFTER_CONTENT_PAUSE:
+                postAdsEvent(AdsListener::onAdDisplayedAfterContentPause);
+                break;
+            case PAUSED:
+                postAdsEvent(L -> L.onAdPaused(((AdEvent.AdPausedEvent) e).adInfo));
+                break;
+            case RESUMED:
+                postAdsEvent(L -> L.onAdResumed(((AdEvent.AdResumedEvent) e).adInfo));
+                break;
+            case COMPLETED:
+                postAdsEvent(AdsListener::onCompleted);
+                break;
+            case FIRST_QUARTILE:
+                postAdsEvent(AdsListener::onFirstQuartile);
+                break;
+            case MIDPOINT:
+                postAdsEvent(AdsListener::onMidpoint);
+                break;
+            case THIRD_QUARTILE:
+                postAdsEvent(AdsListener::onThirdQuartile);
+                break;
+            case SKIPPED:
+                postAdsEvent(L -> L.onAdSkipped(((AdEvent.AdSkippedEvent) e).adInfo));
+                break;
+            case CLICKED:
+                postAdsEvent(AdsListener::onClicked);
+                break;
+            case TAPPED:
+                postAdsEvent(AdsListener::onTapped);
+                break;
+            case ICON_TAPPED:
+                postAdsEvent(AdsListener::onIconTapped);
+                break;
+            case AD_BREAK_READY:
+                postAdsEvent(AdsListener::onIconTapped);
+                break;
+            case AD_PROGRESS:
+                postAdsEvent(AdsListener::onAdProgress);
+                break;
+            case AD_BREAK_STARTED:
+                postAdsEvent(AdsListener::onAdBreakStarted);
+                break;
+            case AD_BREAK_ENDED:
+                postAdsEvent(AdsListener::onAdBreakEnded);
+                break;
+            case AD_BREAK_IGNORED:
+                postAdsEvent(AdsListener::onAdBreakIgnored);
+                break;
+            case CUEPOINTS_CHANGED:
+                postAdsEvent(L -> L.onAdCuePointsUpdate(((AdEvent.AdCuePointsUpdateEvent) e).cuePoints));
+                break;
+            case PLAY_HEAD_CHANGED:
+                postAdsEvent(L -> L.onAdPlayHead(((AdEvent.AdPlayHeadEvent) e).adPlayHead));
+                break;
+            case LOADED:
+                postAdsEvent(L -> L.onAdLoaded(((AdEvent.AdLoadedEvent) e).adInfo));
+                break;
+            case CONTENT_PAUSE_REQUESTED:
+                postAdsEvent(AdsListener::onContentPauseRequested);
+                break;
+            case CONTENT_RESUME_REQUESTED:
+                postAdsEvent(AdsListener::onContentResumeRequested);
+                break;
+            case ALL_ADS_COMPLETED:
+                postAdsEvent(AdsListener::onAllAdsCompleted);
+                break;
+            case AD_LOAD_TIMEOUT_TIMER_STARTED:
+                postAdsEvent(AdsListener::onAdLoadTimeoutTimerStarted);
+                break;
+            case AD_BUFFER_START:
+                postAdsEvent(L -> L.onAdBufferStart(((AdEvent.AdBufferStart) e).adPosition));
+                break;
+            case AD_BUFFER_END:
+                postAdsEvent(L -> L.onAdBufferEnd(((AdEvent.AdBufferEnd) e).adPosition));
+                break;
+            case ERROR:
+                postAdsEvent(L -> L.onError(((AdEvent.Error) e).error));
+                break;
+        }
+    };
+
+    @SuppressWarnings({"deprecation", "FieldCanBeLocal"})
+    final PlayerListener newToLegacyPlayerEvents = new PlayerListener() {
+        @Override
+        public void onError(PKError error) {
+            messageBus.postFromAdapter(new PlayerEvent.Error(error));
+        }
+
+        @Override
+        public void onPlayerStateChanged(PlayerState newState, PlayerState oldState) {
+            messageBus.postFromAdapter(new PlayerEvent.StateChanged(newState, oldState));
+        }
+
+        @Override
+        public void onDurationChanged(long duration) {
+            messageBus.postFromAdapter(new PlayerEvent.DurationChanged(duration));
+        }
+
+        @Override
+        public void onTracksAvailable(PKTracks tracksInfo) {
+            messageBus.postFromAdapter(new PlayerEvent.TracksAvailable(tracksInfo));
+        }
+
+        @Override
+        public void onVolumeChanged(float volume) {
+            messageBus.postFromAdapter(new PlayerEvent.VolumeChanged(volume));
+        }
+
+        @Override
+        public void onPlaybackInfoUpdated(PlaybackInfo playbackInfo) {
+            messageBus.postFromAdapter(new PlayerEvent.PlaybackInfoUpdated(playbackInfo));
+        }
+
+        @Override
+        public void onMetadataAvailable(List<PKMetadata> metadataList) {
+            messageBus.postFromAdapter(new PlayerEvent.MetadataAvailable(metadataList));
+        }
+
+        @Override
+        public void onSourceSelected(PKMediaSource source) {
+            messageBus.postFromAdapter(new PlayerEvent.SourceSelected(source));
+        }
+
+        @Override
+        public void onPlayheadUpdated(long position, long duration) {
+            messageBus.postFromAdapter(new PlayerEvent.PlayheadUpdated(position, duration));
+        }
+
+        @Override
+        public void onSeeking(long targetPosition) {
+            messageBus.postFromAdapter(new PlayerEvent.Seeking(targetPosition));
+        }
+
+        @Override
+        public void onVideoTrackChanged(VideoTrack newTrack) {
+            messageBus.postFromAdapter(new PlayerEvent.VideoTrackChanged(newTrack));
+        }
+
+        @Override
+        public void onAudioTrackChanged(AudioTrack newTrack) {
+            messageBus.postFromAdapter(new PlayerEvent.AudioTrackChanged(newTrack));
+        }
+
+        @Override
+        public void onTextTrackChanged(TextTrack newTrack) {
+            messageBus.postFromAdapter(new PlayerEvent.TextTrackChanged(newTrack));
+        }
+
+        @Override
+        public void onPlaybackRateChanged(float rate) {
+            messageBus.postFromAdapter(new PlayerEvent.PlaybackRateChanged(rate));
+        }
+
+        @Override
+        public void onSubtitlesStyleChanged(String styleName) {
+            messageBus.postFromAdapter(new PlayerEvent.SubtitlesStyleChanged(styleName));
+        }
+
+        @Override
+        public void onCanPlay() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.CAN_PLAY));
+        }
+
+        @Override
+        public void onEnded() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.ENDED));
+        }
+
+        @Override
+        public void onPause() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.PAUSE));
+        }
+
+        @Override
+        public void onPlay() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.PLAY));
+        }
+
+        @Override
+        public void onPlaying() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.PLAYING));
+        }
+
+        @Override
+        public void onSeeked() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.SEEKED));
+        }
+
+        @Override
+        public void onReplay() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.REPLAY));
+        }
+
+        @Override
+        public void onStopped() {
+            messageBus.postFromAdapter(new PlayerEvent.Generic(PlayerEvent.Type.STOPPED));
+        }
+    };
+
+    @SuppressWarnings({"deprecation", "FieldCanBeLocal"})
+    final AdsListener newToLegacyAdsEvents = new AdsListener() {
+        @Override
+        public void onAdFirstPlay() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_FIRST_PLAY));
+        }
+
+        @Override
+        public void onAdDisplayedAfterContentPause() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_DISPLAYED_AFTER_CONTENT_PAUSE));
+        }
+
+        @Override
+        public void onCompleted() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.COMPLETED));
+        }
+
+        @Override
+        public void onFirstQuartile() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.FIRST_QUARTILE));
+        }
+
+        @Override
+        public void onMidpoint() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.MIDPOINT));
+        }
+
+        @Override
+        public void onThirdQuartile() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.THIRD_QUARTILE));
+        }
+
+        @Override
+        public void onClicked() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.CLICKED));
+        }
+
+        @Override
+        public void onTapped() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.TAPPED));
+        }
+
+        @Override
+        public void onIconTapped() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.ICON_TAPPED));
+        }
+
+        @Override
+        public void onAdBreakReady() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_BREAK_READY));
+        }
+
+        @Override
+        public void onAdProgress() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_PROGRESS));
+        }
+
+        @Override
+        public void onAdBreakStarted() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_BREAK_STARTED));
+        }
+
+        @Override
+        public void onAdBreakEnded() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_BREAK_ENDED));
+        }
+
+        @Override
+        public void onAdBreakIgnored() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_BREAK_IGNORED));
+        }
+
+        @Override
+        public void onContentPauseRequested() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.CONTENT_PAUSE_REQUESTED));
+        }
+
+        @Override
+        public void onContentResumeRequested() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.CONTENT_RESUME_REQUESTED));
+        }
+
+        @Override
+        public void onAllAdsCompleted() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.ALL_ADS_COMPLETED));
+        }
+
+        @Override
+        public void onAdLoadTimeoutTimerStarted() {
+            messageBus.postFromAdapter(new AdEvent(AdEvent.Type.AD_LOAD_TIMEOUT_TIMER_STARTED));
+        }
+
+        @Override
+        public void onAdLoaded(AdInfo adInfo) {
+            messageBus.postFromAdapter(new AdEvent.AdLoadedEvent(adInfo));
+        }
+
+        @Override
+        public void onAdStarted(AdInfo adInfo) {
+            messageBus.postFromAdapter(new AdEvent.AdStartedEvent(adInfo));
+        }
+
+        @Override
+        public void onAdPaused(AdInfo adInfo) {
+            messageBus.postFromAdapter(new AdEvent.AdPausedEvent(adInfo));
+        }
+
+        @Override
+        public void onAdResumed(AdInfo adInfo) {
+            messageBus.postFromAdapter(new AdEvent.AdResumedEvent(adInfo));
+        }
+
+        @Override
+        public void onAdSkipped(AdInfo adInfo) {
+            messageBus.postFromAdapter(new AdEvent.AdSkippedEvent(adInfo));
+        }
+
+        @Override
+        public void onAdCuePointsUpdate(AdCuePoints cuePoints) {
+            messageBus.postFromAdapter(new AdEvent.AdCuePointsUpdateEvent(cuePoints));
+        }
+
+        @Override
+        public void onAdPlayHead(long adPlayHead) {
+            messageBus.postFromAdapter(new AdEvent.AdPlayHeadEvent(adPlayHead));
+        }
+
+        @Override
+        public void onAdRequested(String adTagUrl) {
+            messageBus.postFromAdapter(new AdEvent.AdRequestedEvent(adTagUrl));
+        }
+
+        @Override
+        public void onAdBufferStart(long adPosition) {
+            messageBus.postFromAdapter(new AdEvent.AdBufferStart(adPosition));
+        }
+
+        @Override
+        public void onAdBufferEnd(long adPosition) {
+            messageBus.postFromAdapter(new AdEvent.AdBufferEnd(adPosition));
+        }
+
+        @Override
+        public void onError(PKError error) {
+            messageBus.postFromAdapter(new AdEvent.Error(error));
+        }
+    };
+
+    LegacyEventAdapter(MessageBus messageBus) {
+        this.messageBus = messageBus;
+    }
+
+    private void postAdsEvent(Post<AdsListener> post) {
+        messageBus.postAdsEvent(new AdsPost(post));
+    }
+
+    // Proxy to prevent circular forwarding between adapters
+    class AdsPost implements Post<AdsListener> {
+
+        final Post<AdsListener> realPost;
+
+        AdsPost(Post<AdsListener> realPost) {
+            this.realPost = realPost;
+        }
+
+        @Override
+        public void run(AdsListener L) {
+            if (L != newToLegacyAdsEvents) {
+                realPost.run(L);
+            }
+        }
+    }
+
+}
--- playkit/src/main/java/com/kaltura/playkit/MessageBus.java
+++ playkit/src/main/java/com/kaltura/playkit/MessageBus.java
@@ -14,20 +14,13 @@ package com.kaltura.playkit;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.kaltura.playkit.player.AudioTrack;
-import com.kaltura.playkit.player.PKTracks;
-import com.kaltura.playkit.player.TextTrack;
-import com.kaltura.playkit.player.VideoTrack;
-import com.kaltura.playkit.player.metadata.PKMetadata;
-import com.kaltura.playkit.plugins.ads.AdCuePoints;
 import com.kaltura.playkit.plugins.ads.AdEvent;
-import com.kaltura.playkit.plugins.ads.AdInfo;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -41,6 +34,7 @@ public class MessageBus implements Post.Target {
 
     private Handler postHandler = new Handler(Looper.getMainLooper());
     private Map<Object, Set<Object>> listeners;
+    private final LegacyEventAdapter legacyEventAdapter;
 
     enum ListenerType {
         player, ads
@@ -53,13 +47,24 @@ public class MessageBus implements Post.Target {
         listeners.put(ListenerType.player, new HashSet<>(10));
         listeners.put(ListenerType.ads, new HashSet<>(10));
 
-        // The player only sends the new style events, forward them to users of the old events.
-        listeners.get(ListenerType.player).add(legacyPlayerListenerProxy);
-        listeners.get(ListenerType.ads).add(legacyAdsListenerProxy);
+
+        legacyEventAdapter = new LegacyEventAdapter(this);
+
+        // Forward new-style player events to legacy listeners
+        getListeners(ListenerType.player).add(legacyEventAdapter.newToLegacyPlayerEvents);
+
+        // Forward new-style ads events to legacy listeners
+        getListeners(ListenerType.ads).add(legacyEventAdapter.newToLegacyAdsEvents);
+
+        // Forward legacy ads events to new listeners. Player events are only sent in new format.
+        listen(legacyEventAdapter.legacyToNewAdsEvents, AdEvent.Type.values());
     }
 
     public void postPlayerEvent(Post<PlayerListener> post) {
-        final Set<Object> listeners = this.listeners.get(ListenerType.player);
+        final Set<Object> listeners = getListeners(ListenerType.player);
+        if (listeners == null) {
+            return;
+        }
         post(() -> {
             for (Object listener : listeners) {
                 if (listener instanceof PlayerListener) {
@@ -70,7 +75,10 @@ public class MessageBus implements Post.Target {
     }
 
     public void postAdsEvent(Post<AdsListener> post) {
-        final Set<Object> listeners = this.listeners.get(ListenerType.player);
+        final Set<Object> listeners = this.getListeners(ListenerType.ads);
+        if (listeners == null) {
+            return;
+        }
         post(() -> {
             for (Object listener : listeners) {
                 if (listener instanceof AdsListener) {
@@ -90,28 +98,33 @@ public class MessageBus implements Post.Target {
             Log.d(TAG, "LEGACY POSTING EVENT " + event.eventType());
         }
 
-        postInternal(event);
+        postInternal(event, false);
     }
 
-    private void postFromProxy(PKEvent event) {
+    void postFromAdapter(PKEvent event) {
 
-        Log.d(TAG, "PROXY POSTING EVENT " + event.eventType());
+        Log.d(TAG, "ADAPTER POSTING EVENT " + event.eventType());
 
-        postInternal(event);
+        postInternal(event, true);
     }
 
-    private void postInternal(PKEvent event) {
+    private void postInternal(PKEvent event, boolean fromProxy) {
+
         final Set<Object> listeners = this.listeners.get(event.eventType());
 
-        if (listeners != null) {
-            postHandler.post(() -> {
-                for (Object listener : new HashSet<>(listeners)) {
-                    if (listener instanceof PKEvent.Listener) {
-                        ((PKEvent.Listener) listener).onEvent(event);
-                    }
-                }
-            });
+        if (listeners == null) {
+            return;
         }
+        postHandler.post(() -> {
+            for (Object listener : new HashSet<>(listeners)) {
+                if (listener instanceof PKEvent.Listener) {
+                    if (fromProxy && listener == legacyEventAdapter.legacyToNewAdsEvents) {
+                        continue;
+                    }
+                    ((PKEvent.Listener) listener).onEvent(event);
+                }
+            }
+        });
     }
 
     public void remove(PKEvent.Listener listener, Enum... eventTypes) {
@@ -150,279 +163,14 @@ public class MessageBus implements Post.Target {
     }
 
     public void listen(PlayerListener listener) {
-        listeners.get(ListenerType.player).add(listener);
+        getListeners(ListenerType.player).add(listener);
+    }
+
+    private Set<Object> getListeners(ListenerType type) {
+        return listeners.get(type);
     }
 
     public void listen(AdsListener listener) {
-        listeners.get(ListenerType.ads).add(listener);
+        getListeners(ListenerType.ads).add(listener);
     }
-
-
-
-    @SuppressWarnings({"deprecation", "FieldCanBeLocal"})
-    private PlayerListener legacyPlayerListenerProxy = new PlayerListener() {
-        @Override
-        public void onError(PKError error) {
-            postFromProxy(new PlayerEvent.Error(error));
-        }
-
-        @Override
-        public void onPlayerStateChanged(PlayerState newState, PlayerState oldState) {
-            postFromProxy(new PlayerEvent.StateChanged(newState, oldState));
-        }
-
-        @Override
-        public void onDurationChanged(long duration) {
-            postFromProxy(new PlayerEvent.DurationChanged(duration));
-        }
-
-        @Override
-        public void onTracksAvailable(PKTracks tracksInfo) {
-            postFromProxy(new PlayerEvent.TracksAvailable(tracksInfo));
-        }
-
-        @Override
-        public void onVolumeChanged(float volume) {
-            postFromProxy(new PlayerEvent.VolumeChanged(volume));
-        }
-
-        @Override
-        public void onPlaybackInfoUpdated(PlaybackInfo playbackInfo) {
-            postFromProxy(new PlayerEvent.PlaybackInfoUpdated(playbackInfo));
-        }
-
-        @Override
-        public void onMetadataAvailable(List<PKMetadata> metadataList) {
-            postFromProxy(new PlayerEvent.MetadataAvailable(metadataList));
-        }
-
-        @Override
-        public void onSourceSelected(PKMediaSource source) {
-            postFromProxy(new PlayerEvent.SourceSelected(source));
-        }
-
-        @Override
-        public void onPlayheadUpdated(long position, long duration) {
-            postFromProxy(new PlayerEvent.PlayheadUpdated(position, duration));
-        }
-
-        @Override
-        public void onSeeking(long targetPosition) {
-            postFromProxy(new PlayerEvent.Seeking(targetPosition));
-        }
-
-        @Override
-        public void onVideoTrackChanged(VideoTrack newTrack) {
-            postFromProxy(new PlayerEvent.VideoTrackChanged(newTrack));
-        }
-
-        @Override
-        public void onAudioTrackChanged(AudioTrack newTrack) {
-            postFromProxy(new PlayerEvent.AudioTrackChanged(newTrack));
-        }
-
-        @Override
-        public void onTextTrackChanged(TextTrack newTrack) {
-            postFromProxy(new PlayerEvent.TextTrackChanged(newTrack));
-        }
-
-        @Override
-        public void onPlaybackRateChanged(float rate) {
-            postFromProxy(new PlayerEvent.PlaybackRateChanged(rate));
-        }
-
-        @Override
-        public void onSubtitlesStyleChanged(String styleName) {
-            postFromProxy(new PlayerEvent.SubtitlesStyleChanged(styleName));
-        }
-
-        @Override
-        public void onCanPlay() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.CAN_PLAY));
-        }
-
-        @Override
-        public void onEnded() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.ENDED));
-        }
-
-        @Override
-        public void onPause() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.PAUSE));
-        }
-
-        @Override
-        public void onPlay() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.PLAY));
-        }
-
-        @Override
-        public void onPlaying() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.PLAYING));
-        }
-
-        @Override
-        public void onSeeked() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.SEEKED));
-        }
-
-        @Override
-        public void onReplay() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.REPLAY));
-        }
-
-        @Override
-        public void onStopped() {
-            postFromProxy(new PlayerEvent.Generic(PlayerEvent.Type.STOPPED));
-        }
-    };
-
-    @SuppressWarnings({"deprecation", "FieldCanBeLocal"})
-    private AdsListener legacyAdsListenerProxy = new AdsListener() {
-        @Override
-        public void onAdFirstPlay() {
-            post(new AdEvent(AdEvent.Type.AD_FIRST_PLAY));
-        }
-
-        @Override
-        public void onAdDisplayedAfterContentPause() {
-            post(new AdEvent(AdEvent.Type.AD_DISPLAYED_AFTER_CONTENT_PAUSE));
-        }
-
-        @Override
-        public void onCompleted() {
-            post(new AdEvent(AdEvent.Type.COMPLETED));
-        }
-
-        @Override
-        public void onFirstQuartile() {
-            post(new AdEvent(AdEvent.Type.FIRST_QUARTILE));
-        }
-
-        @Override
-        public void onMidpoint() {
-            post(new AdEvent(AdEvent.Type.MIDPOINT));
-        }
-
-        @Override
-        public void onThirdQuartile() {
-            post(new AdEvent(AdEvent.Type.THIRD_QUARTILE));
-        }
-
-        @Override
-        public void onClicked() {
-            post(new AdEvent(AdEvent.Type.CLICKED));
-        }
-
-        @Override
-        public void onTapped() {
-            post(new AdEvent(AdEvent.Type.TAPPED));
-        }
-
-        @Override
-        public void onIconTapped() {
-            post(new AdEvent(AdEvent.Type.ICON_TAPPED));
-        }
-
-        @Override
-        public void onAdBreakReady() {
-            post(new AdEvent(AdEvent.Type.AD_BREAK_READY));
-        }
-
-        @Override
-        public void onAdProgress() {
-            post(new AdEvent(AdEvent.Type.AD_PROGRESS));
-        }
-
-        @Override
-        public void onAdBreakStarted() {
-            post(new AdEvent(AdEvent.Type.AD_BREAK_STARTED));
-        }
-
-        @Override
-        public void onAdBreakEnded() {
-            post(new AdEvent(AdEvent.Type.AD_BREAK_ENDED));
-        }
-
-        @Override
-        public void onAdBreakIgnored() {
-            post(new AdEvent(AdEvent.Type.AD_BREAK_IGNORED));
-        }
-
-        @Override
-        public void onContentPauseRequested() {
-            post(new AdEvent(AdEvent.Type.CONTENT_PAUSE_REQUESTED));
-        }
-
-        @Override
-        public void onContentResumeRequested() {
-            post(new AdEvent(AdEvent.Type.CONTENT_RESUME_REQUESTED));
-        }
-
-        @Override
-        public void onAllAdsCompleted() {
-            post(new AdEvent(AdEvent.Type.ALL_ADS_COMPLETED));
-        }
-
-        @Override
-        public void onAdLoadTimeoutTimerStarted() {
-            post(new AdEvent(AdEvent.Type.AD_LOAD_TIMEOUT_TIMER_STARTED));
-        }
-
-        @Override
-        public void onAdLoaded(AdInfo adInfo) {
-            post(new AdEvent.AdLoadedEvent(adInfo));
-        }
-
-        @Override
-        public void onAdStarted(AdInfo adInfo) {
-            post(new AdEvent.AdStartedEvent(adInfo));
-        }
-
-        @Override
-        public void onAdPaused(AdInfo adInfo) {
-            post(new AdEvent.AdPausedEvent(adInfo));
-        }
-
-        @Override
-        public void onAdResumed(AdInfo adInfo) {
-            post(new AdEvent.AdResumedEvent(adInfo));
-        }
-
-        @Override
-        public void onAdSkipped(AdInfo adInfo) {
-            post(new AdEvent.AdSkippedEvent(adInfo));
-        }
-
-        @Override
-        public void onAdCuePointsUpdate(AdCuePoints cuePoints) {
-            post(new AdEvent.AdCuePointsUpdateEvent(cuePoints));
-        }
-
-        @Override
-        public void onAdPlayHead(long adPlayHead) {
-            post(new AdEvent.AdPlayHeadEvent(adPlayHead));
-        }
-
-        @Override
-        public void onAdRequested(String adTagUrl) {
-            post(new AdEvent.AdRequestedEvent(adTagUrl));
-        }
-
-        @Override
-        public void onAdBufferStart(long adPosition) {
-            post(new AdEvent.AdBufferStart(adPosition));
-        }
-
-        @Override
-        public void onAdBufferEnd(long adPosition) {
-            post(new AdEvent.AdBufferEnd(adPosition));
-        }
-
-        @Override
-        public void onError(PKError error) {
-            post(new AdEvent.Error(error));
-        }
-    };
-
 }
