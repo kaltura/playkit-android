26fd204c377caf6854d824a4f91f25faf4dec0a9
Gilad Nadav <Gilad.Nadav@kaltura.com>
2018-12-18 20:59:39 +0200

    enable playback of offline content if drmdata is provided for clear content

--- playkit/src/main/java/com/kaltura/playkit/drm/SimpleDashParser.java
+++ playkit/src/main/java/com/kaltura/playkit/drm/SimpleDashParser.java
@@ -85,11 +85,9 @@ class SimpleDashParser {
             if (format != null) {
                 drmInitData = format.drmInitData;
             }
-            if (drmInitData == null) {
-                throw new IOException("drmInitData is not initialized");
-            }
-            hasContentProtection = drmInitData.schemeDataCount > 0;
-            if (hasContentProtection) {
+            
+            if (drmInitData != null && drmInitData.schemeDataCount > 0) {
+                hasContentProtection = drmInitData.schemeDataCount > 0;
                 loadDrmInitData(representation);
             } else {
                 log.i("no content protection found");
