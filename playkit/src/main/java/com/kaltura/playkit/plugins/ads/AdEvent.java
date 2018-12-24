d7afb6d05dcf1be611e054c20a39fbc46908ac91
Gilad Nadav <gilad.nadav@kaltura.com>
2018-12-24 15:29:46 +0200

    Add AdClickedEvent with clickThruUrl overload

--- playkit/src/main/java/com/kaltura/playkit/plugins/ads/AdEvent.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/ads/AdEvent.java
@@ -29,6 +29,7 @@ public class AdEvent implements PKEvent {
     public static final Class<AdBufferStart> adBufferStart = AdBufferStart.class;
     public static final Class<AdBufferEnd> adBufferEnd = AdBufferEnd.class;
     public static final Class<AdPlaybackInfoUpdated> adPlaybackInfoUpdated = AdPlaybackInfoUpdated.class;
+    public static final Class<AdClickedEvent> adClickedEvent = AdClickedEvent.class;
     public static final Class<Error> error = Error.class;
 
     public static final AdEvent.Type adFirstPlay = Type.AD_FIRST_PLAY;
@@ -38,7 +39,6 @@ public class AdEvent implements PKEvent {
     public static final AdEvent.Type midpoint = Type.MIDPOINT;
     public static final AdEvent.Type thirdQuartile = Type.THIRD_QUARTILE;
     public static final AdEvent.Type skippableStateChanged = Type.SKIPPABLE_STATE_CHANGED;
-    public static final AdEvent.Type clicked = Type.CLICKED;
     public static final AdEvent.Type tapped = Type.TAPPED;
     public static final AdEvent.Type iconTapped = Type.ICON_TAPPED;
     public static final AdEvent.Type adBreakReady = Type.AD_BREAK_READY;
@@ -135,6 +135,16 @@ public class AdEvent implements PKEvent {
         public AdRequestedEvent(String adTagUrl) {
             super(Type.AD_REQUESTED);
             this.adTagUrl = adTagUrl;
+        }
+    }
+
+    public static class AdClickedEvent extends AdEvent {
+
+        public final String clickThruUrl;
+
+        public AdClickedEvent(String clickThruUrl) {
+            super(Type.CLICKED);
+            this.clickThruUrl = clickThruUrl;
         }
     }
 
