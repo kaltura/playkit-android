cc9d27de4a59075b98146170bec168258b692882
Noam Tamim <noam.tamim@kaltura.com>
2019-02-18 14:26:34 +0200

    CustomRendererFactory cleanup
    Remove the fields because their values are already passed to us in buildVideoRenderers().

--- playkit/src/main/java/com/kaltura/playkit/player/CustomRendererFactory.java
+++ playkit/src/main/java/com/kaltura/playkit/player/CustomRendererFactory.java
@@ -18,26 +18,18 @@ import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 import java.util.ArrayList;
 
-/**
- * Created by anton.afanasiev on 25/02/2018.
- */
-
 public class CustomRendererFactory extends DefaultRenderersFactory {
-
-    private boolean allowClearLead;
-    private long allowedVideoJoiningTimeMs;
 
     public CustomRendererFactory(Context context, boolean allowClearLead, long allowedVideoJoiningTimeMs) {
         super(context);
-        this.allowClearLead = allowClearLead;
-        this.allowedVideoJoiningTimeMs = allowedVideoJoiningTimeMs;
+        setAllowedVideoJoiningTimeMs(allowedVideoJoiningTimeMs);
         setPlayClearSamplesWithoutKeys(allowClearLead);
     }
 
     @Override
     protected void buildVideoRenderers(Context context, int extensionRendererMode, MediaCodecSelector mediaCodecSelector, @Nullable DrmSessionManager<FrameworkMediaCrypto> drmSessionManager, boolean playClearSamplesWithoutKeys, Handler eventHandler, VideoRendererEventListener eventListener, long allowedVideoJoiningTimeMs, ArrayList<Renderer> out) {
         out.add(new CustomVideoCodecRenderer(context, MediaCodecSelector.DEFAULT,
-                this.allowedVideoJoiningTimeMs, drmSessionManager, allowClearLead, eventHandler, eventListener,
+                allowedVideoJoiningTimeMs, drmSessionManager, playClearSamplesWithoutKeys, eventHandler, eventListener,
                 MAX_DROPPED_VIDEO_FRAME_COUNT_TO_NOTIFY));
     }
 }
