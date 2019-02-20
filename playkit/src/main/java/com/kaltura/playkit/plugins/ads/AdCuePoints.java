678ced0814d2e8cb845c68a3d41c8ee064f4483c
Gilad Nadav <gilad.nadav@kaltura.com>
2019-02-20 03:28:07 +0200

    add hint for plugin name

--- playkit/src/main/java/com/kaltura/playkit/plugins/ads/AdCuePoints.java
+++ playkit/src/main/java/com/kaltura/playkit/plugins/ads/AdCuePoints.java
@@ -12,8 +12,6 @@
 
 package com.kaltura.playkit.plugins.ads;
 
-import android.util.Pair;
-
 import java.util.List;
 
 /**
@@ -23,9 +21,18 @@ import java.util.List;
 public class AdCuePoints {
 
     private List<Long> adCuePoints;
+    private String adPluginName = "";
 
     public AdCuePoints(List<Long> adCuePoints) {
         this.adCuePoints = adCuePoints;
+    }
+
+    public String getAdPluginName() {
+        return adPluginName;
+    }
+
+    public void setAdPluginName(String adPluginName) {
+        this.adPluginName = adPluginName;
     }
 
     public List<Long> getAdCuePoints() {
