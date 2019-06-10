54679e4df394c2d2bc368cd5edc731e0bf3936ec
Gilad Nadav <gilad.nadav@kaltura.com>
2019-06-10 13:51:02 +0300

    add copy constructor to convert from mediaEntry to VR mediaEntry

--- playkit/src/main/java/com/kaltura/playkit/player/vr/VRPKMediaEntry.java
+++ playkit/src/main/java/com/kaltura/playkit/player/vr/VRPKMediaEntry.java
@@ -4,7 +4,30 @@ import com.kaltura.playkit.PKMediaEntry;
 
 public class VRPKMediaEntry extends PKMediaEntry {
 
-    public VRPKMediaEntry() {
-        super();
+    public VRPKMediaEntry() {}
+
+    public VRPKMediaEntry(PKMediaEntry mediaEntry) {
+        if (mediaEntry == null) {
+            return;
+        }
+        if (mediaEntry.getId() != null) {
+            this.setId(mediaEntry.getId());
+        }
+        if (mediaEntry.getName() != null) {
+            this.setName(mediaEntry.getName());
+        }
+        if (mediaEntry.getSources() != null) {
+            this.setSources(mediaEntry.getSources());
+        }
+        this.setDuration(mediaEntry.getDuration());
+        if (mediaEntry.getMediaType() != null) {
+            this.setMediaType(mediaEntry.getMediaType());
+        }
+        if (mediaEntry.getExternalSubtitleList() != null) {
+            this.setExternalSubtitleList(mediaEntry.getExternalSubtitleList());
+        }
+        if (mediaEntry.getMetadata() != null) {
+            this.setMetadata(mediaEntry.getMetadata());
+        }
     }
 }
