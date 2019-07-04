32aa70d6b13753cbd3938f94ca49943223a53c0f
Gilad Nadav <gilad.nadav@kaltura.com>
2019-07-04 13:26:37 +0300

    remove unused method

--- playkit/src/main/java/com/kaltura/playkit/player/PlayerSettings.java
+++ playkit/src/main/java/com/kaltura/playkit/player/PlayerSettings.java
@@ -253,26 +253,4 @@ public class PlayerSettings implements Player.Settings {
         this.vrSettings = vrSettings;
         return this;
     }
-
-    private boolean isVRModeSupported(Context context, VRInteractionMode mode) {
-        switch (mode) {
-            case Touch:
-                //Always supported
-                return true;
-            case Motion:
-            case MotionWithTouch:
-                SensorManager motionSensorManager = (SensorManager) context
-                        .getSystemService(Context.SENSOR_SERVICE);
-                return motionSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null;
-            case CardboardMotion:
-            case CardboardMotionWithTouch:
-                SensorManager cardboardSensorManager = (SensorManager) context
-                        .getSystemService(Context.SENSOR_SERVICE);
-                Sensor accelerometerSensor = cardboardSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-                Sensor gyroSensor = cardboardSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-                return accelerometerSensor != null && gyroSensor != null;
-            default:
-                return true;
-        }
-    }
 }
