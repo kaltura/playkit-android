b46412ad03de385fa131738e2004402f260cde74
Gilad Nadav <gilad.nadav@kaltura.com>
2019-11-10 19:00:43 +0200

    FEM-2706 use custom load control instead f using given LoadControl builder (#586)
    * use CustonLoadControl instead of using using  DefaultLoadControl builder
    incase no change in the Playkit LoadControl value we will use ExoPlayer's DefaultLoadControl

--- playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
+++ playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
@@ -50,11 +50,13 @@ import com.kaltura.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.kaltura.android.exoplayer2.ui.SubtitleView;
 import com.kaltura.android.exoplayer2.upstream.BandwidthMeter;
 import com.kaltura.android.exoplayer2.upstream.DataSource;
+import com.kaltura.android.exoplayer2.upstream.DefaultAllocator;
 import com.kaltura.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.kaltura.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.kaltura.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.kaltura.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.kaltura.android.exoplayer2.upstream.HttpDataSource;
+import com.kaltura.android.exoplayer2.video.CustomLoadControl;
 import com.kaltura.playkit.*;
 import com.kaltura.playkit.drm.DeferredDrmSessionManager;
 import com.kaltura.playkit.drm.DrmCallback;
@@ -208,15 +210,19 @@ public class ExoPlayerWrapper implements PlayerEngine, Player.EventListener, Met
             return customLoadControlStrategy.getCustomLoadControl();
         } else {
             final LoadControlBuffers loadControl = playerSettings.getLoadControlBuffers();
-            int backBufferDurationMs = loadControl.getBackBufferDurationMs();
-            boolean retainBackBufferFromKeyframe = loadControl.getRetainBackBufferFromKeyframe();
-            return new DefaultLoadControl.Builder().
-                    setBufferDurationsMs(
-                            loadControl.getMinPlayerBufferMs(),
-                            loadControl.getMaxPlayerBufferMs(),
-                            loadControl.getMinBufferAfterInteractionMs(),
-                            loadControl.getMinBufferAfterReBufferMs()).
-                    setBackBuffer(backBufferDurationMs, retainBackBufferFromKeyframe).createDefaultLoadControl();
+            if (!loadControl.isDefaultValuesModified()) {
+                return new DefaultLoadControl();
+            }
+            return new CustomLoadControl(new DefaultAllocator(/* trimOnReset= */ true, C.DEFAULT_BUFFER_SEGMENT_SIZE),
+                    loadControl.getMinPlayerBufferMs(),
+                    loadControl.getMaxPlayerBufferMs(), // minBufferVideoMs is set same as the maxBufferMs due to issue in exo player FEM-2707
+                    loadControl.getMaxPlayerBufferMs(),
+                    loadControl.getMinBufferAfterInteractionMs(),
+                    loadControl.getMinBufferAfterReBufferMs(),
+                    DefaultLoadControl.DEFAULT_TARGET_BUFFER_BYTES,
+                    DefaultLoadControl.DEFAULT_PRIORITIZE_TIME_OVER_SIZE_THRESHOLDS,
+                    loadControl.getBackBufferDurationMs(),
+                    loadControl.getRetainBackBufferFromKeyframe());
         }
     }
 
