7f107db7105cc2fc2cb7ecaeffd4fd01998dd5a7
Gilad Nadav <gilad.nadav@kaltura.com>
2020-09-16 10:41:56 +0300

    cleanup

--- playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
+++ playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
@@ -73,7 +73,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import okhttp3.OkHttpClient;
 
