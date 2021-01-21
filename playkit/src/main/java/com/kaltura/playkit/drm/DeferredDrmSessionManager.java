c57b5794f604d5a09be1ecc61d61fbcd1eb190ce
Gilad Nadav <gilad.nadav@kaltura.com>
2021-01-21 18:42:25 +0200

    error log

--- playkit/src/main/java/com/kaltura/playkit/drm/DeferredDrmSessionManager.java
+++ playkit/src/main/java/com/kaltura/playkit/drm/DeferredDrmSessionManager.java
@@ -152,7 +152,7 @@ public class DeferredDrmSessionManager implements DrmSessionManager, DrmSessionE
                                 frameworkMediaDrm.setPropertyString(SECURITY_LEVEL_PROPERTY, WIDEVINE_SECURITY_LEVEL_3);
                                 return frameworkMediaDrm;
                             } catch (UnsupportedDrmException e) {
-                                log.d("ForceWidevineL3Playback failed due to " + e.getMessage());
+                                log.e("ForceWidevineL3Playback failed due to " + e.getMessage());
                                 return FrameworkMediaDrm.DEFAULT_PROVIDER.acquireExoMediaDrm(MediaSupport.WIDEVINE_UUID);
                             }
                         });
