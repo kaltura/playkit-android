1782cd7a17322ec9a05ba517bd711d880946dd00
Gourav Saxena <gourav.saxena@kaltura.com>
2021-04-16 12:33:16 +0530

    - ExoPlayer upgrade to v2.13.3

--- playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
+++ playkit/src/main/java/com/kaltura/playkit/player/ExoPlayerWrapper.java
@@ -91,7 +91,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
