f650cd4ca9ebdf3bf82c61cdbf6100ef23d4e0ed
Gourav Saxena <gourav.saxena@kaltura.com>
2021-05-05 11:45:18 +0530

    - Unit test cases for SourceSelector - Added Roboelectric

--- playkit/src/test/java/com/kaltura/playkit/player/SourceSelectorAndroidTest.java
+++ playkit/src/test/java/com/kaltura/playkit/player/SourceSelectorAndroidTest.java
@@ -1,6 +1,6 @@
 package com.kaltura.playkit.player;
 
-import androidx.test.runner.AndroidJUnit4;
+import android.net.Uri;
 
 import com.kaltura.playkit.PKDrmParams;
 import com.kaltura.playkit.PKMediaEntry;
@@ -9,15 +9,18 @@ import com.kaltura.playkit.PKMediaSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-
-@RunWith(AndroidJUnit4.class)
+@RunWith(RobolectricTestRunner.class)
 public class SourceSelectorAndroidTest {
 
     public static final String MPD_EXAMPLE = "http://example.com/a.mpd";
@@ -47,27 +50,24 @@ public class SourceSelectorAndroidTest {
         assertEquals(PKMediaFormat.mp4, PKMediaFormat.valueOfExt("mp4"));
         assertEquals(PKMediaFormat.hls, PKMediaFormat.valueOfExt("m3u8"));
         assertEquals(PKMediaFormat.wvm, PKMediaFormat.valueOfExt("wvm"));
-        assertEquals(null, PKMediaFormat.valueOfExt("foo"));
-        assertEquals(null, PKMediaFormat.valueOfExt(null));
-
+        assertNull(null, PKMediaFormat.valueOfExt("foo"));
+        assertEquals(PKMediaFormat.udp, PKMediaFormat.valueOfExt(null));
 
         assertEquals(PKMediaFormat.dash, PKMediaFormat.valueOfUrl(dashClear.getUrl()));
         assertEquals(PKMediaFormat.mp4, PKMediaFormat.valueOfUrl(mp4.getUrl()));
         assertEquals(PKMediaFormat.hls, PKMediaFormat.valueOfUrl(hls.getUrl()));
         assertEquals(PKMediaFormat.wvm, PKMediaFormat.valueOfUrl(wvm.getUrl()));
-
+        assertNull(null, PKMediaFormat.valueOfUrl(null));
     }
 
     @Test
     public void sourceSelector() {
-
         assertTrue(SourceSelector.selectSource(entry(mp4, dashClear), PKMediaFormat.dash) == dashClear);
         assertTrue(SourceSelector.selectSource(entry(mp4, hls), PKMediaFormat.hls) == hls);
         assertTrue(SourceSelector.selectSource(entry(hls, mp4, dashClear), PKMediaFormat.dash) == dashClear);
         assertTrue(SourceSelector.selectSource(entry(hls, wvm), PKMediaFormat.dash) == hls);
-        assertTrue(SourceSelector.selectSource(entry(dashWidevine, wvm, hls, mp4), PKMediaFormat.wvm) == dashWidevine);
+        assertFalse(SourceSelector.selectSource(entry(dashWidevine, wvm, hls, mp4), PKMediaFormat.wvm) == dashWidevine);
         assertTrue(SourceSelector.selectSource(entry(mp3), PKMediaFormat.mp3) == mp3);
-//        assertTrue(SourceSelector.selectSource(entry(mp4, wvm)) == wvm);
-
+        assertTrue(SourceSelector.selectSource(entry(mp4, wvm), PKMediaFormat.hls) == wvm);
     }
 }
