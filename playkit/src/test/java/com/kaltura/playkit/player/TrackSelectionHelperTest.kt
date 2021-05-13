26e6f56d992d60c084385f63825fe871f54d6722
Gourav Saxena <gourav.saxena@kaltura.com>
2021-05-13 19:14:59 +0530

    - Cleanup

--- playkit/src/test/java/com/kaltura/playkit/player/TrackSelectionHelperTest.kt
+++ playkit/src/test/java/com/kaltura/playkit/player/TrackSelectionHelperTest.kt
@@ -1,15 +1,10 @@
 package com.kaltura.playkit.player
 
-import org.junit.runner.RunWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
 internal class TrackSelectionHelperTest {
-    
-    @InjectMocks
-    var trackSelectionHelper: TrackSelectionHelper? = null
+//
+//    @InjectMocks
+//    var trackSelectionHelper: TrackSelectionHelper? = null
 
 
 }
