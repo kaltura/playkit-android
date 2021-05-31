20a4c747d867bac71f0539625b657107c417d143
Gilad Nadav <gilad.nadav@kaltura.com>
2021-05-31 12:59:18 +0300

    adding explanation for attributes

--- playkit/src/main/java/com/kaltura/playkit/player/MulticastSettings.java
+++ playkit/src/main/java/com/kaltura/playkit/player/MulticastSettings.java
@@ -2,14 +2,15 @@ package com.kaltura.playkit.player;
 
 public class MulticastSettings {
 
+    // whether mulicast playback will use exo default config or using app user config
     private boolean useExoDefaultSettings = true;
     // maxPacketSize The maximum datagram packet size, in bytes.
     private int maxPacketSize = 3000;
     // socketTimeoutMillis The socket timeout in milliseconds. A timeout of zero is interpreted
     private int socketTimeoutMillis = 10000;
-
+    //Modes for the extractor. One of MODE_MULTI_PMT, MODE_SINGLE_PMT, MODE_HLS
     private ExtractorMode extractorMode = ExtractorMode.MODE_MULTI_PMT;
-
+    //The desired value of the first adjusted sample timestamp in microseconds - for no offset give MAX_LONG
     private long firstSampleTimestampUs;
     
     enum ExtractorMode {
