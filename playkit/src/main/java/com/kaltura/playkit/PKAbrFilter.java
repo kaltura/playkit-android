d9ffdb2c535f56ead9ed6d7b76ff2e4fc1fdd3ee
Gourav Saxena <gourav.saxena@kaltura.com>
2021-06-07 17:44:22 +0530

    - Removed redundant keys

--- playkit/src/main/java/com/kaltura/playkit/PKAbrFilter.java
+++ playkit/src/main/java/com/kaltura/playkit/PKAbrFilter.java
@@ -6,8 +6,4 @@ public enum PKAbrFilter {
     HEIGHT,
     WIDTH,
     PIXEL;
-
-    public static final String KEY_ABR_TYPE = "abrType";
-    public static final String KEY_ABR_MIN = "abrMin";
-    public static final String KEY_ABR_MAX = "abrMax";
 }
