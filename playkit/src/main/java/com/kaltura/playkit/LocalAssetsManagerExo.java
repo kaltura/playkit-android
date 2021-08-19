fd9fc19bdbf656212c96ffeb431edbd4fd2246e3
Gourav Saxena <gourav.saxena@kaltura.com>
2021-08-19 18:51:06 +0530

    - Changed API name internally

--- playkit/src/main/java/com/kaltura/playkit/LocalAssetsManagerExo.java
+++ playkit/src/main/java/com/kaltura/playkit/LocalAssetsManagerExo.java
@@ -48,7 +48,7 @@ public class LocalAssetsManagerExo {
         }
     }
 
-    public void registerWidevineDashAsset(String assetId, String licenseUri, byte[] drmInitData, boolean forceWidevineL3Playback) throws LocalAssetsManager.RegisterException {
+    public void registerWidevineAsset(String assetId, String licenseUri, byte[] drmInitData, boolean forceWidevineL3Playback) throws LocalAssetsManager.RegisterException {
         final WidevineModularAdapter widevine = new WidevineModularAdapter(helper.context, helper.localDataStore);
         widevine.registerAsset(drmInitData, "video/mp4", licenseUri, forceWidevineL3Playback, helper.licenseRequestParamAdapter);
         helper.saveMediaFormat(assetId, PKMediaFormat.dash, PKDrmParams.Scheme.WidevineCENC);
