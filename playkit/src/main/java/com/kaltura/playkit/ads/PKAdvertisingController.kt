1ef0e5e7d2f17219537c853a4b619637559c17b2
Gourav Saxena <gourav.saxena@kaltura.com>
2022-01-24 14:59:07 +0530

    - Log's movement

--- playkit/src/main/java/com/kaltura/playkit/ads/PKAdvertisingController.kt
+++ playkit/src/main/java/com/kaltura/playkit/ads/PKAdvertisingController.kt
@@ -605,28 +605,26 @@ class PKAdvertisingController: PKAdvertising, IMAEventsListener {
         isAllAdsFailed?.let {
             if (hasWaterFallingAds) {
                 if (it) {
-                    log.e("Firing AdWaterFallingFailed event")
                     messageBus?.post(
                         AdEvent.AdWaterFallingFailed(adBreakConfig)
                     )
                     // When the all ads in the waterfalling list fail then with `AdWaterFallingFailed`, we need to fire
                     // AdEvent.error as well so that Analytics behaves as usual
-                    log.e("Firing AdEvent.error after AdWaterFallingFailed")
                     error?.let { err ->
                         messageBus?.post(err)
                     }
-
+                    log.e("Firing AdEvent.error after AdWaterFallingFailed event")
                 } else {
-                    log.d("Firing AdWaterFalling event")
                     messageBus?.post(
                         AdEvent.AdWaterFalling(adBreakConfig)
                     )
+                    log.d("Firing AdWaterFalling event")
                 }
             } else {
-                log.e("Firing AdError because there was no AdWaterFalling")
                 error?.let { err ->
                     messageBus?.post(err)
                 }
+                log.e("Firing AdError because there was no AdWaterFalling")
             }
             hasWaterFallingAds = false
             return
